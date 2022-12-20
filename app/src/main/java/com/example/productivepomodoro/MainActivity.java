4fa5d4fb321ed68f438aa2fa79df79e3f760790d
Subash <it.subashaa@gmail.com>
2022-12-20 12:37:24 +0530

    init test

--- app/src/main/java/com/example/productivepomodoro/MainActivity.java
+++ app/src/main/java/com/example/productivepomodoro/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.productivepomodoro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        text.setText("Productive Pomodoro");
     }
 }
